dd7f84cdedf7d5e8d17fa9d99c158a3c6730b703
facu <facundozumpano@gmail.com>
2022-11-15 21:39:03 -0300

    task 4 con Vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,117 +1,71 @@
-fetch('https://amazing-events.herokuapp.com/api/events')
-    .then(response => response.json())
-    .then(eventsFetch => {
-        const allEvents = eventsFetch.events
-        const currentDate = eventsFetch.currentDate
-        const pastEvents = createPastEvents(allEvents, currentDate)
-        const upcomingEvents = createUpcomingEvents(allEvents, currentDate)
-        const allCategories = allEvents.map((e) => e.category)
-        const categories = new Set(allCategories)
-        const arrayCategories = Array.from(categories)
-        if (form) printCategories(arrayCategories, form)
-        home ? printCards(allEvents, 'home-cards') :
-        past ? printCards(pastEvents, 'past-cards') :
-        upcoming ? printCards(upcomingEvents, 'upcoming-cards') : ''
-        if (form) {
-            form.addEventListener('change', () => {
-                let checked = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(e => e.value)
-                let searchValue = search.value.toLowerCase().trim()
-                if (home != null) {
-                    let eventFilter = checkboxFilter(allEvents, checked)
-                    let searchFilterr = searchFilter(eventFilter, searchValue)
-                    printCards(searchFilterr, 'home-cards')
-                } else if (past != null) {
-                    let eventFilter = checkboxFilter(pastEvents, checked)
-                    let searchFilterr = searchFilter(eventFilter, searchValue)
-                    printCards(searchFilterr, 'past-cards')
-                } else if (upcoming != null) {
-                    let eventFilter = checkboxFilter(upcomingEvents, checked)
-                    let searchFilterr = searchFilter(eventFilter, searchValue)
-                    printCards(searchFilterr, 'upcoming-cards')
-                }
-            })
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            events: [],
+            filterEvents: [],
+            currentDate: '',
+            pastEvents: [],
+            filterPastEvents: [],
+            upcomingEvents: [],
+            filterUpcomingEvents: [],
+            categories: [],
+            checked: [],
+            searchInput: ''
         }
-        if (search) {
-            search.addEventListener('keyup', () => {
-                let checked = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(e => e.value)
-                let searchValue = search.value.toLowerCase().trim()
-                if (document.getElementById('home-cards') != null) {
-                    let eventFilter = checkboxFilter(allEvents, checked)
-                    let searchFilterr = searchFilter(eventFilter, searchValue)
-                    printCards(searchFilterr, 'home-cards')
-                } else if (document.getElementById('past-cards') != null) {
-                    let eventFilter = checkboxFilter(pastEvents, checked)
-                    let searchFilterr = searchFilter(eventFilter, searchValue)
-                    printCards(searchFilterr, 'past-cards')
-                } else if (document.getElementById('upcoming-cards') != null) {
-                    let eventFilter = checkboxFilter(upcomingEvents, checked)
-                    let searchFilterr = searchFilter(eventFilter, searchValue)
-                    printCards(searchFilterr, 'upcoming-cards')
-                }
+    },
+    created() {
+        fetch('https://amazing-events.herokuapp.com/api/events')
+            .then(response => response.json())
+            .then(response => {
+                this.events = response.events
+                this.filterEvents = this.events
+                this.currentDate = response.currentDate
+                this.createPastEvents()
+                this.createUpcomingEvents()
+                this.createCategories()
             })
+            .catch(error => console.log(error))
+    },
+    methods: {
+        createPastEvents() {
+            this.pastEvents = this.events.filter((e) => e.date < this.currentDate)
+            this.filterPastEvents = this.events.filter((e) => e.date < this.currentDate)
+        },
+        createUpcomingEvents() {
+            this.upcomingEvents = this.events.filter((e) => e.date > this.currentDate)
+            this.filterUpcomingEvents = this.events.filter((e) => e.date > this.currentDate)
+        },
+        createCategories() {
+            this.categories = [... new Set(this.events.map((e) => e.category))]
         }
-    })
+    },
+    computed: {
+        filteringEvents() {
 
-function printCards(events, id) {
-    let fragment = new DocumentFragment()
-    const cardsContainer = document.getElementById(id)
-    cardsContainer.innerHTML = ''
-    events.forEach((e) => {
-        let div = document.createElement('div')
-        div.classList.add('card')
-        div.innerHTML = `<div>
-            <img class="card-img" src=${e.image} alt="${e.category}">
-        </div>
-        <div>
-        <div class="card-title">
-            <h3>${e.name}</h3>
-            <p>${e.description}</p>
-        </div>
-        <div class="card-info">
-        <div>
-            <p>Price: $ ${e.price}</p>
-        </div>
-        <div class="more-info">
-            <p><a href="./details.html?id=${e._id}">More Info</a></p>
-        </div>
-    </div>
-</div>`
-        fragment.appendChild(div)
-    })
-    return cardsContainer.appendChild(fragment)
-}
+            const filterCheckbox = this.events.filter(e => this.checked.includes(e.category))
+            if (filterCheckbox.length === 0) {
+                this.filterEvents = this.events.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }else{
+                this.filterEvents = filterCheckbox.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }
 
-function createPastEvents(events, currentDate) {
-    const pastEvents = events.filter((e) => e.date < currentDate)
-    return pastEvents
-}
+            const pastCheckbox = this.pastEvents.filter(e => this.checked.includes(e.category))
+            this.filterPastEvents = pastCheckbox.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            if (pastCheckbox.length === 0) {
+                this.filterPastEvents = this.pastEvents.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }else{
+                this.filterEvents = filterCheckbox.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }
 
-function createUpcomingEvents(events, currentDate) {
-    const upcomingEvents = events.filter((e) => e.date > currentDate)
-    return upcomingEvents
-}
-
-function printCategories(categories, container) {
-    let checkboxes = ''
-    categories.forEach((e) =>
-        checkboxes += `<div class="main.form">
-    <input type="checkbox" value="${e}" name="${e.split(' ').join('').toLowerCase()}" id="${e.split(' ').join('').toLowerCase()}" checked>
-    <label for="${e.split(' ').join('').toLowerCase()}">${e}</label>
-    </div>`);
-    container.innerHTML += checkboxes
-}
-
-function checkboxFilter(events, checked) {
-    let filterEvents = events.filter(e => checked.includes(e.category))
-    return filterEvents
-}
-function searchFilter(events, searchValue) {
-    let filterSearch = events.filter(e => e.name.toLowerCase().trim().includes(searchValue))
-    return filterSearch
-}
-
-const form = document.getElementById('main-form')
-const search = document.getElementById('search')
-const home = document.getElementById('home-cards')
-const past = document.getElementById('past-cards')
-const upcoming = document.getElementById('upcoming-cards')
+            const upcomingCheckbox = this.upcomingEvents.filter(e => this.checked.includes(e.category))
+            this.filterUpcomingEvents = upcomingCheckbox.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            if (upcomingCheckbox.length === 0) {
+                this.filterUpcomingEvents = this.upcomingEvents.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }else{
+                this.filterEvents = filterCheckbox.filter(e => e.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }
+        }
+    }
+}).mount('#app')
